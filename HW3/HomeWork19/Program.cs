53570905e95874b79b9805845bef8004ad4f1994
SteelPlated <mix.singatulin@yandex.ru>
2022-05-23 14:51:58 +0500

    домашка

---
+++ HW3/HomeWork19/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на
+вход пятизначное число и проверяет, является ли оно
+палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да*/
+
+Console.Write("Введите число: ");
+        bool palindrome = true;
+        string x = Console.ReadLine();
+        int c = x.Length - 1;
+            for (int i = 0; i < c; i++)
+            if (x[i] != x[c - i])
+                palindrome = false;
+        if (palindrome == true)
+            Console.Write("Да");
+        else Console.Write("Нет");
